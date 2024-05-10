edeb39e2999843c8827a20ea9ffa26afebb0c3f7
JB-Tellez <jb.tellez@gmail.com>
2024-05-10 10:18:45 -0700

    tidies up url use in data module

--- app/data.ts
+++ app/data.ts
@@ -2,22 +2,24 @@ import { json } from "@remix-run/node";
 import Snack from "./types/snack-record";
 import SnackData from "./types/snack-data";
 
+const API_URL = process.env.API_URL ?? '';
+
 export async function getSnacks() {
-    const url = process.env.API_URL ?? '';
+    const url = API_URL;
     const response = await fetch(url);
     const data = await response.json();
     return data.data;
 }
 
 export async function getSnack(id:number) {
-    const url = process.env.API_URL ?? '';
-    const response = await fetch(`${url}/${id}`);
+    const url = `${API_URL}/${id}`;
+    const response = await fetch(url);
     const data = await response.json();
     return data.data[0];
 }
 
 export async function createSnack(snackData: SnackData) {
-    const url:string = process.env.API_URL ?? '';
+    const url = API_URL;
     const response = await fetch(url, {
         method: 'POST',
         body: JSON.stringify(snackData),
@@ -30,7 +32,7 @@ export async function createSnack(snackData: SnackData) {
 }
 
 export async function deleteSnack(id:number) {
-    const url = `${process.env.API_URL}/${id}`;
+    const url = `${API_URL}/${id}`;
     const response = await fetch(url, {
         method: 'DELETE'
     });
